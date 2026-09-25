c58d6194d8e5c734b9b3e22d51473a7f162edf4b
Gabriel Dias <70488342+gabriel0223@users.noreply.github.com>
2026-09-25 14:45:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,4 @@
 window.SUPABASE_URL      = "https://nqahboaqthcfozpofeej.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_4gix_I3VLBWa42Fd1tvZIw_tPoA4_S7";
 window.VAPID_PUBLIC_KEY = "BDkwfgdXAMnzoHFShYqn9KGMnOdfvLyx5P2Vd8cZKzleSA8Qi6exrrEnZyy6Fx-kcJV6mnSZSAWG-7kAhkIL3JE";
+window.MEET_AT = "2027-05-01T22:00:00+09:00";
